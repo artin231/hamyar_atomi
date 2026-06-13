f214f2752dde620c719a99e5478b85089249cf1c
artin231 <leiartrezjah@gmail.com>
2026-06-14 00:28:39 +0330

    changing the db model

--- modeling_db/drawSQL-sqlsrv-export-2026-06-13.sql
+++ modeling_db/drawSQL-sqlsrv-export-2026-06-13.sql
@@ -26,7 +26,8 @@ CREATE TABLE "azmoon"(
     "time" VARCHAR(255) NOT NULL,
     "type" VARCHAR(255) NOT NULL,
     "head_nomre" INT NOT NULL,
-    "teacher_id" INT NOT NULL
+    "teacher_id" INT NOT NULL,
+    "student" INT NULL
 );
 ALTER TABLE
     "azmoon" ADD CONSTRAINT "azmoon_id_primary" PRIMARY KEY("id");
@@ -35,37 +36,18 @@ CREATE TABLE "soal"(
     "azmoon_id" INT NOT NULL,
     "title" VARCHAR(255) NOT NULL,
     "is_did" VARBINARY(MAX) NOT NULL,
-    "nomre" INT NOT NULL
+    "nomre" INT NOT NULL,
+    "gozine_student" INT NOT NULL
 );
 ALTER TABLE
     "soal" ADD CONSTRAINT "soal_id_primary" PRIMARY KEY("id");
 CREATE TABLE "gozine_soal"(
     "id" INT NOT NULL,
     "soal_id" INT NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
-    "is_clicked" VARBINARY(MAX) NOT NULL
+    "title" VARCHAR(255) NOT NULL
 );
 ALTER TABLE
     "gozine_soal" ADD CONSTRAINT "gozine_soal_id_primary" PRIMARY KEY("id");
-CREATE TABLE "jajvab_azmoon"(
-    "id" INT NOT NULL,
-    "student_code" INT NOT NULL,
-    "azmoon_id" INT NOT NULL,
-    "nomre" INT NOT NULL
-);
-ALTER TABLE
-    "jajvab_azmoon" ADD CONSTRAINT "jajvab_azmoon_id_primary" PRIMARY KEY("id");
-CREATE TABLE "soal_javab"(
-    "id" INT NOT NULL,
-    "javab_azmoon_id" INT NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
-    "is_did" VARBINARY(MAX) NOT NULL,
-    "gozine" INT NOT NULL,
-    "is_correct" VARBINARY(MAX) NOT NULL,
-    "nomre" INT NOT NULL
-);
-ALTER TABLE
-    "soal_javab" ADD CONSTRAINT "soal_javab_id_primary" PRIMARY KEY("id");
 CREATE TABLE "teacher"(
     "teacher_code" INT NOT NULL,
     "name" VARCHAR(255) NOT NULL,
@@ -108,17 +90,11 @@ ALTER TABLE
 CREATE TABLE "apcense"(
     "id" INT NOT NULL,
     "student_id" INT NOT NULL,
-    "time" VARCHAR(255) NOT NULL
+    "time" VARCHAR(255) NOT NULL,
+    "reason" VARCHAR(255) NULL
 );
 ALTER TABLE
     "apcense" ADD CONSTRAINT "apcense_id_primary" PRIMARY KEY("id");
-CREATE TABLE "apsence_reason"(
-    "id" INT NOT NULL,
-    "student_id" INT NOT NULL,
-    "apsence_id" INT NOT NULL
-);
-ALTER TABLE
-    "apsence_reason" ADD CONSTRAINT "apsence_reason_id_primary" PRIMARY KEY("id");
 CREATE TABLE "homework"(
     "id" INT NOT NULL,
     "title" VARCHAR(255) NOT NULL,
@@ -162,60 +138,50 @@ CREATE TABLE "ekhtar"(
 ALTER TABLE
     "ekhtar" ADD CONSTRAINT "ekhtar_id_primary" PRIMARY KEY("id");
 ALTER TABLE
-    "karname" ADD CONSTRAINT "karname_student_code_foreign" FOREIGN KEY("student_code") REFERENCES "student"("student_code");
-ALTER TABLE
-    "comments" ADD CONSTRAINT "comments_id_foreign" FOREIGN KEY("id") REFERENCES "personel"("persenel_code");
-ALTER TABLE
-    "grade" ADD CONSTRAINT "grade_student_id_foreign" FOREIGN KEY("student_id") REFERENCES "student"("student_code");
-ALTER TABLE
-    "student" ADD CONSTRAINT "student_student_code_foreign" FOREIGN KEY("student_code") REFERENCES "grade"("student_id");
-ALTER TABLE
-    "soal_javab" ADD CONSTRAINT "soal_javab_javab_azmoon_id_foreign" FOREIGN KEY("javab_azmoon_id") REFERENCES "jajvab_azmoon"("id");
-ALTER TABLE
-    "rooidad" ADD CONSTRAINT "rooidad_grade_foreign" FOREIGN KEY("grade") REFERENCES "grade"("id");
+    "class" ADD CONSTRAINT "class_teacher_code_foreign" FOREIGN KEY("teacher_code") REFERENCES "teacher"("teacher_code");
 ALTER TABLE
     "grade" ADD CONSTRAINT "grade_id_foreign" FOREIGN KEY("id") REFERENCES "grade"("id");
 ALTER TABLE
-    "gozine_soal" ADD CONSTRAINT "gozine_soal_soal_id_foreign" FOREIGN KEY("soal_id") REFERENCES "soal"("azmoon_id");
+    "grade" ADD CONSTRAINT "grade_teacher_id_foreign" FOREIGN KEY("teacher_id") REFERENCES "teacher"("teacher_code");
 ALTER TABLE
-    "apcense" ADD CONSTRAINT "apcense_student_id_foreign" FOREIGN KEY("student_id") REFERENCES "student"("student_code");
+    "comments" ADD CONSTRAINT "comments_id_foreign" FOREIGN KEY("id") REFERENCES "personel"("persenel_code");
 ALTER TABLE
-    "apsence_reason" ADD CONSTRAINT "apsence_reason_apsence_id_foreign" FOREIGN KEY("apsence_id") REFERENCES "apcense"("id");
+    "class" ADD CONSTRAINT "class_students_foreign" FOREIGN KEY("students") REFERENCES "student"("student_code");
+ALTER TABLE
+    "azmoon" ADD CONSTRAINT "azmoon_grade_foreign" FOREIGN KEY("grade") REFERENCES "grade"("id");
+ALTER TABLE
+    "grade" ADD CONSTRAINT "grade_student_id_foreign" FOREIGN KEY("student_id") REFERENCES "student"("student_code");
 ALTER TABLE
     "grade" ADD CONSTRAINT "grade_id_foreign" FOREIGN KEY("id") REFERENCES "azmoon"("grade");
 ALTER TABLE
-    "student" ADD CONSTRAINT "student_student_code_foreign" FOREIGN KEY("student_code") REFERENCES "class"("students");
+    "student" ADD CONSTRAINT "student_student_code_foreign" FOREIGN KEY("student_code") REFERENCES "grade"("student_id");
+ALTER TABLE
+    "apcense" ADD CONSTRAINT "apcense_student_id_foreign" FOREIGN KEY("student_id") REFERENCES "student"("student_code");
 ALTER TABLE
     "grade" ADD CONSTRAINT "grade_id_foreign" FOREIGN KEY("id") REFERENCES "homework"("grade_id");
 ALTER TABLE
     "azmoon" ADD CONSTRAINT "azmoon_teacher_id_foreign" FOREIGN KEY("teacher_id") REFERENCES "teacher"("teacher_code");
 ALTER TABLE
-    "jajvab_azmoon" ADD CONSTRAINT "jajvab_azmoon_azmoon_id_foreign" FOREIGN KEY("azmoon_id") REFERENCES "azmoon"("id");
-ALTER TABLE
-    "nomre" ADD CONSTRAINT "nomre_karname_id_foreign" FOREIGN KEY("karname_id") REFERENCES "karname"("id");
-ALTER TABLE
-    "grade" ADD CONSTRAINT "grade_teacher_id_foreign" FOREIGN KEY("teacher_id") REFERENCES "teacher"("teacher_code");
+    "news" ADD CONSTRAINT "news_grade_foreign" FOREIGN KEY("grade") REFERENCES "grade"("id");
 ALTER TABLE
     "teacher" ADD CONSTRAINT "teacher_teacher_code_foreign" FOREIGN KEY("teacher_code") REFERENCES "grade"("teacher_id");
 ALTER TABLE
-    "jajvab_azmoon" ADD CONSTRAINT "jajvab_azmoon_student_code_foreign" FOREIGN KEY("student_code") REFERENCES "student"("student_code");
-ALTER TABLE
-    "grade" ADD CONSTRAINT "grade_id_foreign" FOREIGN KEY("id") REFERENCES "rooidad"("grade");
-ALTER TABLE
-    "class" ADD CONSTRAINT "class_students_foreign" FOREIGN KEY("students") REFERENCES "student"("student_code");
-ALTER TABLE
-    "apsence_reason" ADD CONSTRAINT "apsence_reason_student_id_foreign" FOREIGN KEY("student_id") REFERENCES "student"("student_code");
-ALTER TABLE
-    "azmoon" ADD CONSTRAINT "azmoon_grade_foreign" FOREIGN KEY("grade") REFERENCES "grade"("id");
+    "student" ADD CONSTRAINT "student_student_code_foreign" FOREIGN KEY("student_code") REFERENCES "class"("students");
 ALTER TABLE
     "homework" ADD CONSTRAINT "homework_grade_id_foreign" FOREIGN KEY("grade_id") REFERENCES "grade"("id");
 ALTER TABLE
-    "ekhtar" ADD CONSTRAINT "ekhtar_id_foreign" FOREIGN KEY("id") REFERENCES "student"("student_code");
-ALTER TABLE
-    "class" ADD CONSTRAINT "class_teacher_code_foreign" FOREIGN KEY("teacher_code") REFERENCES "teacher"("teacher_code");
+    "gozine_soal" ADD CONSTRAINT "gozine_soal_soal_id_foreign" FOREIGN KEY("soal_id") REFERENCES "soal"("azmoon_id");
 ALTER TABLE
     "soal" ADD CONSTRAINT "soal_azmoon_id_foreign" FOREIGN KEY("azmoon_id") REFERENCES "azmoon"("id");
 ALTER TABLE
-    "news" ADD CONSTRAINT "news_grade_foreign" FOREIGN KEY("grade") REFERENCES "grade"("id");
-ALTER TABLE
     "news" ADD CONSTRAINT "news_personel_id_foreign" FOREIGN KEY("personel_id") REFERENCES "personel"("persenel_code");
+ALTER TABLE
+    "rooidad" ADD CONSTRAINT "rooidad_grade_foreign" FOREIGN KEY("grade") REFERENCES "grade"("id");
+ALTER TABLE
+    "grade" ADD CONSTRAINT "grade_id_foreign" FOREIGN KEY("id") REFERENCES "rooidad"("grade");
+ALTER TABLE
+    "nomre" ADD CONSTRAINT "nomre_karname_id_foreign" FOREIGN KEY("karname_id") REFERENCES "karname"("id");
+ALTER TABLE
+    "ekhtar" ADD CONSTRAINT "ekhtar_id_foreign" FOREIGN KEY("id") REFERENCES "student"("student_code");
+ALTER TABLE
+    "karname" ADD CONSTRAINT "karname_student_code_foreign" FOREIGN KEY("student_code") REFERENCES "student"("student_code");
