e8912bd2bb235798212aa2a9aa7bf36822f18e91
artin231 <leiartrezjah@gmail.com>
2026-07-09 20:59:57 +0330

    i added the created the home page template and changing the settings with django

--- hamyar_atomi/hamyar_atomi/settings.py
+++ hamyar_atomi/hamyar_atomi/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'hamyar_atomi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
