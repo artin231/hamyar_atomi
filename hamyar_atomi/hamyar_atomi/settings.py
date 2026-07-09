b88819fc0a3ed4c905e9d8a57eaffaf49d8f6379
artin231 <leiartrezjah@gmail.com>
2026-07-09 21:10:16 +0330

    starting the home page

--- hamyar_atomi/hamyar_atomi/settings.py
+++ hamyar_atomi/hamyar_atomi/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'hamyar_atomi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates',BASE_DIR / 'home' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
